1f53333c2b8652f51fbcada6767c3bd0fb600e08
christopherbanas <cjb5537@rit.edu>
2023-10-22 21:41:35 -0400

    Updated on 10/22/23

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import os
 import time
+import errno
 from shutil import copyfileobj
 
 from requests import get
 from nba_api.stats.endpoints import commonteamroster
 from nba_api.stats.static.teams import get_teams
 
-CURRENT_SEASON = '2022-23'
+CURRENT_SEASON = '2023-24'
+
+
+def create_path(path: str):
+    if os.path.exists(path):
+        return
+
+    try:
+        os.makedirs(path, exist_ok=True)  # Python>3.2
+    except TypeError:
+        try:
+            os.makedirs(path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
 
 
 def main():
@@ -29,6 +46,7 @@ def main():
 
             raw_file = get(headshot_link, stream=True)
             destination_path = f"{os.path.dirname(__file__)}/images/{CURRENT_SEASON}/{player_id}-{team_id}.png"
+            create_path(f"{os.path.dirname(__file__)}/images/{CURRENT_SEASON}")
 
             if raw_file.status_code == 200:
                 print("\t\tHeadshot found")
